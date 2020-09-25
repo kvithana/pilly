600fe45b6db0d6dcc86b27fb93636d05cb6daa09
Marc <marcporciuncula@gmail.com>
2020-09-25 21:37:31 +1000

    💅 Run formatter

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,21 +1,21 @@
-import { createBrowserHistory } from "history";
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./App";
-import "./index.css";
-import * as serviceWorker from "./serviceWorker";
-import "./styles/generated/tailwind.css";
+import { createBrowserHistory } from 'history'
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './App'
+import './index.css'
+import * as serviceWorker from './serviceWorker'
+import './styles/generated/tailwind.css'
 
-const history = createBrowserHistory();
+const history = createBrowserHistory()
 
 ReactDOM.render(
   <React.StrictMode>
     <App history={history} />
   </React.StrictMode>,
-  document.getElementById("root")
-);
+  document.getElementById('root'),
+)
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+serviceWorker.unregister()
