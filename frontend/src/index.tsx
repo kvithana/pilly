95c83ce3f5bf23d9b83726b0259664f69edf842f
Kalana Vithana <kalanavithana1@gmail.com>
2020-09-27 09:31:36 +1000

    🤯 service worker force takeover

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -19,7 +19,23 @@ ReactDOM.render(
   document.getElementById('root'),
 )
 
+const onUpdateReady = (registration: ServiceWorkerRegistration) => {
+  const waitingServiceWorker = registration.waiting
+
+  if (waitingServiceWorker) {
+    waitingServiceWorker.addEventListener('statechange', (event) => {
+      // @ts-ignore
+      if (event?.target?.state === 'activated') {
+        window.location.reload()
+      }
+    })
+    waitingServiceWorker.postMessage({ type: 'SKIP_WAITING' })
+  }
+}
+
+const config = { onUpdate: onUpdateReady }
+
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.register()
+serviceWorker.register(config)
