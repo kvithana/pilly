c49fd6c18c1ac5ecddfa5917d207e7747e1baa66
Marc <marcporciuncula@gmail.com>
2020-09-26 19:49:03 +1000

    😇 fix login

--- frontend/src/contexts/AuthContext.tsx
+++ frontend/src/contexts/AuthContext.tsx
@@ -1,7 +1,6 @@
 import React, { useEffect, useState } from 'react'
-import firebase from 'firebase/app'
-import 'firebase/auth'
-import { auth, functions } from '../firebase'
+import { firebase, auth, functions } from '../firebase'
+
 export const AuthContext = React.createContext<{
   currentUser: firebase.User | null
   isPending: boolean
@@ -15,7 +14,7 @@ export const AuthProvider = ({ children }: { children: React.ReactNode }) => {
     const minDelay = delay(1000)
 
     const unsub = auth.onAuthStateChanged((user) => {
-      Promise.all([minDelay, ensureUser()]).then(() => {
+      Promise.all([minDelay, user ? ensureUser() : Promise.resolve()]).then(() => {
         setCurrentUser(user)
         setPending(false)
       })
--- frontend/src/views/login.tsx
+++ frontend/src/views/login.tsx
@@ -1,25 +1,23 @@
 // Import FirebaseAuth and firebase.
 import React, { useMemo } from 'react'
 import StyledFirebaseAuth from 'react-firebaseui/StyledFirebaseAuth'
+import { Button } from '../components'
 import { cs } from '../cs'
 import { auth, firebase } from '../firebase'
 
+const provider = new firebase.auth.GoogleAuthProvider()
+
 export const LoginView = ({ onLoginSuccess }: { onLoginSuccess: () => void }) => {
-  const config: firebaseui.auth.Config = useMemo(
-    () => ({
-      // Popup signin flow rather than redirect flow.
-      signInFlow: 'redirect',
-      // Redirect to /signedIn after sign in is successful. Alternatively you can provide a callbacks.signInSuccess function.
-      signInSuccess: onLoginSuccess,
-      // We will display Google and Facebook as auth providers.
-      signInOptions: [firebase.auth.GoogleAuthProvider.PROVIDER_ID],
-    }),
-    [onLoginSuccess],
-  )
+  const login = () => {
+    auth.signInWithPopup(provider).then((result) => {
+      console.log(result)
+      onLoginSuccess()
+    })
+  }
 
   return (
     <div className={cs('container', 'mx-auto')}>
-      <StyledFirebaseAuth uiConfig={config} firebaseAuth={auth} />
+      <Button onClick={login}>Sign in with Google</Button>
     </div>
   )
 }
